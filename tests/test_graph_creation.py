7ab7a21d22e4050f9829285e40f46acef122a527
EmmyCodes <emingram49@gmail.com>
2022-06-27 14:46:56 -0600

    initial creation tests complete

--- tests/test_graph_creation.py
+++ tests/test_graph_creation.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from hypothesis import given
 from hypothesis.strategies import lists, integers
 from networkf import graph
