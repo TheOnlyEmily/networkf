7f2cbb69a3b4ddede25acecf77993bdf2175f45a
EmmyCodes <emingram49@gmail.com>
2022-06-27 15:16:41 -0600

    implemented single node addition

--- networkf/graph.py
+++ networkf/graph.py
@@ -1,4 +1,10 @@
+from typing import Any
+from toolz.dicttoolz import assoc
+
 graph = dict
 
 def create_empty() -> graph:
     return dict()
+
+def add_node(graph: graph, node: Any) -> graph:
+    return assoc(graph, node, list())
