8d65cbebd93be48e51309f633c49d3e864c64416
EmmyCodes <emingram49@gmail.com>
2022-06-27 08:04:00 -0600

    initial testing commit

--- tests/test_graph_creation.py
+++ tests/test_graph_creation.py
@@ -3,26 +3,33 @@ from hypothesis.strategies import lists, integers
 from networkf import graph
 
 def test_create_empty_graph():
-    graph = graph.create_empty()
-    assert len(graph) == 0
+    g = graph.create_empty()
+    assert len(g) == 0
 
 @given(integers())
 def test_add_node(node):
-    graph = graph.create_empty()
-    new_graph = graph.add_node(graph, node)
-    assert len(new_graph) == 1
+    g = graph.create_empty()
+    new_g = graph.add_node(g, node)
+    assert len(new_g) == 1
+    assert node in new_g
 
 @given(integers(), integers())
 def test_add_edge(n1, n2):
-    graph = graph.create_empty()
-    new_graph = graph.add_edge(graph, n1, n2)
-    assert len(new_graph) > 0
-    assert len(new_graph) <= 2
+    g = graph.create_empty()
+    new_g = graph.add_edge(g, n1, n2)
+    assert len(new_g) > 0
+    assert len(new_g) <= 2
+    assert n1 in new_g
+    assert n2 in new_g
 
 @given(lists(elements=integers()))
 def test_create_from_nodes(nodes):
-    graph = graph.from_nodes(*nodes)
+    g = graph.from_nodes(*nodes)
+    assert len(g) == len(nodes)
+    assert all(n in g for n in nodes)
 
-@given(lists(elements=lists(integers(), min_size=2, max_size=2)))
+@given(lists(elements=lists(integers())))
 def test_create_from_edges(edges):
-    graph = graph.from_edges(*edges)
+    g = graph.from_edges(*edges)
+    assert len(g) > 0
+    assert len(g) <= 2 * len(edges)
