b6b02b5ed8ec12039d2f40dfad392d82af3f4dab
EmmyCodes <emingram49@gmail.com>
2022-07-02 19:46:42 -0600

    decouple data and functions

--- tests/test_graph_creation.py
+++ tests/test_graph_creation.py
@@ -16,7 +16,6 @@ def test_add_node(node):
     new_g = graph.add_node(g, node)
     assert len(new_g) == 1
     assert node in new_g
-    assert new_g[node] == []
 
 @given(integers(), integers())
 def test_add_edge(n1, n2):
